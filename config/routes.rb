473a2ee43c24eec3d66ac3b2a083e73519f1afe0
Ryuunoske <ryuunoske@dsmc.co.in>
2015-06-17 16:18:22 +0530

    ch4

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
+  get 'ch3/home'
+
+  get 'ch3/help'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'ch3#home'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
