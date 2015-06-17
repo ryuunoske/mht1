84c6d861d1b066845ba7ef50c142b7c34137f7a2
Ryuunoske <ryuunoske@dsmc.co.in>
2015-06-17 17:14:30 +0530

    Users Model

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,17 @@
 Rails.application.routes.draw do
+  get 'signup' => 'users#new'
+
+  get "home" => 'ch5#home'
+
+  get "help" => 'ch5#help'
+
+  get "about" => 'ch5#about'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'ch5#home'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
